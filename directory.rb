d323f209f892ced040678812a6945c9ad647d75f
Camelia <camelia.elkhalidy@gmail.com>
2017-06-28 13:42:26 +0100

    interactive menu - cleaning code

--- directory.rb
+++ directory.rb
@@ -1,20 +1,31 @@
+@students = [] # an empty array accessible to all methods
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
 end
 
 def print_header
@@ -22,33 +33,28 @@ def print_header
   puts "-------------"
 end
 
-def print_students(students)
-  students.each do |student|
+def print_students
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
+
 def interactive_menu
-  students = []
   loop do
-   # 1. print the menu and ask the user what to do
-   puts "1. Input the students"
-   puts "2. Show the students"
-   puts "9. Exit"
+   print_menu
    # 2. read the input and save it into a variable
    selection = gets.chomp
    # 3. do what the user has asked
    case selection
    when "1"
-      students = input_students
+      input_students
    when "2"
-      print_header
-      print_students(students)
-      print_footer(students)
+      show_students
    when "9"
       exit # this will cause the program to terminate
    else
