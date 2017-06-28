048b00d097f74766be6697ccaaab84d7d0066375
Camelia <camelia.elkhalidy@gmail.com>
2017-06-28 12:53:44 +0100

    cleaned code to proceed on step 9

--- directory.rb
+++ directory.rb
@@ -1,48 +1,39 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create a students array
+  # create an empty array
   students = []
-  #collect name
-  name = gets.chomp.capitalize
+  # get the first name
+  name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp.capitalize
+    name = gets.chomp
   end
   # return the array of students
   students
 end
-
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_students (students)
-  index = 0
-  until index >= students.count
-    student = students [index]
-    puts "#{student [:name]} (#{student [:cohort]} cohort)"
-    index = index+1
+def print_students(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
-
-  #students.each_with_index do |student, index|
-  #  if student[:name].length < 12
-  #    puts "#{index+1}. #{student [:name]} (#{student [:cohort]} cohort)"
-  #  end
-  #end
 end
 
-def print_footer (adults)
-  puts "Overall we have #{adults.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-idiots = input_students
+students = input_students
+#nothing happens until we call the methods
 print_header
-print_students (idiots)
-print_footer (idiots)
+print_students(students)
+print_footer(students)
